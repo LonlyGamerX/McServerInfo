41e3be7c9aed1f18ec5a99564d7748b17642135f
scott <technomusicgaming117@gmail.com>
2022-09-08 13:42:49 +0200

    Added basic files

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,11 @@
+import "./Dashboard.css";
+
+const Dashboard: React.FC = () => {
+  return (
+    <div className="dashboard">
+      <h1>Dashboard</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/pages/Ranks.tsx
@@ -0,0 +1,11 @@
+import "./Ranks.css";
+
+const Ranks: React.FC = () => {
+  return (
+    <div className="contact">
+      <h1>Contact</h1>
+    </div>
+  );
+};
+
+export default Ranks;
